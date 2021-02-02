26b820ee2f62d1752564c6c2e5af7dbdd40f298e
adrianiliev11 <adrianiliev11@gmail.com>
2021-02-02 13:42:04 +0200

    fix

---
+++ src/app/custom/StarWarsUniverse.js
@@ -0,0 +1,18 @@
+import Entity from '../custom/Entity';
+
+export default class StarWarsUniverse{
+    constructor(){
+        this.entities = this.init().then(response => this.entities = response)
+    }
+    async init(){
+      const response = await fetch('https://swapi.booost.bg/api/');
+      const data = await response.json();
+      const arrayEntities = Object.entries(data);
+      let results = [];
+      for(let i = 0; i < 6; i++){
+          const entity = new Entity(arrayEntities[`${i}`][0], arrayEntities[`${i}`][1]);
+          results = results.concat(entity);
+      }
+      return results;
+    }
+  }
